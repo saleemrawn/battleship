304508d457417ed2a664c31444aedc193db479b2
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-09-12 13:50:31 +0100

    feat: add func to render gameboard to DOM

---
+++ src/dom-handler.js
@@ -0,0 +1,31 @@
+export function renderGameboardToDOM(gameboard, playerID) {
+  const mainContainer = document.querySelector("main");
+
+  mainContainer.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="player-container" data-player-id="${playerID}">
+        <div class="overlay hide"></div>
+    </div>
+    `
+  );
+
+  const playerContainer = document.querySelector(
+    `.player-container[data-player-id="${playerID}"]`
+  );
+
+  const html = gameboard
+    .map((row, i) =>
+      row
+        .map(
+          (elem, j) =>
+            `<button class="board-square${
+              elem !== null ? " ship" : ""
+            }" data-player-id="${playerID}" data-x="${i}" data-y="${j}"></button>`
+        )
+        .join("")
+    )
+    .join("");
+
+  playerContainer.insertAdjacentHTML("beforeend", html);
+}
