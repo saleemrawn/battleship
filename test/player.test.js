a349bca0f422216a7f83a0800d2729d64aa7527a
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-10-06 12:06:34 +0100

    test: remove test cases related to player type
    - Prop type no longer required for player obj

--- test/player.test.js
+++ test/player.test.js
@@ -1,15 +1,5 @@
 import createPlayer from "../src/player";
 
-test("Player obj has type property with value 0 for real player", () => {
-  const newPlayer = createPlayer(0);
-  expect(newPlayer).toHaveProperty("type", 0);
-});
-
-test("Player obj has type property with value 1 for computer player", () => {
-  const newPlayer = createPlayer(1);
-  expect(newPlayer).toHaveProperty("type", 1);
-});
-
 test("Player obj has gameboard property with val gameboard obj", () => {
   const newPlayer = createPlayer(0);
   expect(newPlayer).toHaveProperty("gameboard", expect.any(Object));
