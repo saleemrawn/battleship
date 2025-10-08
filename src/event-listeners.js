f9c0527c6dfe9d346a3ce70202b5ada7a401bba5
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-10-08 13:50:30 +0100

    refactor: update player/computer handler routes
    - update event handler routes through gameEventController

--- src/event-listeners.js
+++ src/event-listeners.js
@@ -9,17 +9,18 @@ import {
   clearGameboards,
 } from "./dom-handler";
 import createPlayer from "./player";
+import { gameEventController } from ".";
 
 let human = createPlayer(1, "Player");
 let computer = createPlayer(2, "Computer");
 
 export function loadEventListeners() {
   addGlobalEventListener("click", `.board-square[data-player-id="2"]`, (event) => {
-    handlePlayerEvent(event, computer);
+    gameEventController.handlePlayerEvent(human, computer, event);
   });
 
   addGlobalEventListener("mouseup", `.board-square[data-player-id="2"]`, () => {
-    handleComputerEvent(human);
+    gameEventController.handleComputerEvent(human, computer);
   });
 
   addGlobalEventListener("click", "#dialogPlayAgain", () => handlePlayAgainEvent(human, computer));
