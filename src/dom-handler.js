0b29458f403a193c80470da9be0dac64b81aeb41
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-10-10 12:13:39 +0100

    refactor: remove renderGameboardShips, moved to shipPlacementUI

--- src/dom-handler.js
+++ src/dom-handler.js
@@ -1,16 +1,3 @@
-export function renderGameboardShips(gameboard, playerID) {
-  for (let i = 0; i < gameboard.length; i++) {
-    for (let j = 0; j < gameboard[i].length; j++) {
-      if (gameboard[i][j] !== null) {
-        let boardButton = document.querySelector(
-          `.board-square[data-player-id="${playerID}"][data-x="${i}"][data-y="${j}"]`
-        );
-        boardButton.classList.add("ship");
-      }
-    }
-  }
-}
-
 export function clearGameboards() {
   document.querySelector("main").innerHTML = "";
 }
