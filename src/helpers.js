71fcef33c2475640c9260ef07833e216969744ea
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-10-14 12:03:12 +0100

    feat: add helper to get ship information

--- src/helpers.js
+++ src/helpers.js
@@ -5,3 +5,15 @@ export function generateRandomCoordinate(boardSize) {
 export function generateRandomOrientation() {
   return Math.round(Math.random()) === 0 ? "horizontal" : "vertical";
 }
+
+export function getShipsInformation() {
+  const ships = [
+    { name: "Patrol Boat", length: 2 },
+    { name: "Submarine", length: 3 },
+    { name: "Destroyer", length: 3 },
+    { name: "Battleship", length: 4 },
+    { name: "Carrier", length: 5 },
+  ];
+
+  return ships;
+}
