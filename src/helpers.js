8dd1c5936bc3925ce3a0349a86952794ed9a4a6d
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-10-14 12:01:57 +0100

    refactor: update param name to boardSize for clarity

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,3 @@
-export function generateRandomCoordinate(gridSize) {
-  return Math.floor(Math.random() * gridSize);
+export function generateRandomCoordinate(boardSize) {
+  return Math.floor(Math.random() * boardSize);
 }
