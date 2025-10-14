504315d0212b4e25a82462af98e04cd407cd9e6a
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-10-14 12:02:32 +0100

    feat: add helper to generate random orientation

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,7 @@
 export function generateRandomCoordinate(boardSize) {
   return Math.floor(Math.random() * boardSize);
 }
+
+export function generateRandomOrientation() {
+  return Math.round(Math.random()) === 0 ? "horizontal" : "vertical";
+}
