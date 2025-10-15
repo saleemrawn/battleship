4e6f35722b148c315f65ffa16131768c77703e23
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-10-15 15:22:26 +0100

    test: refactor tests and update tests for gameboard
    - update test using describe blocks for obj methods
    - update createGameboard import path
    - remove unnecessary test

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,73 +1,68 @@
-import createGameboard from "../src/gameboard";
+import createGameboard from "../src/js/gameboard";
 
-test("Gameboard obj has gameboard array ", () => {
-  const newGameboard = createGameboard();
-  expect(newGameboard.board).toEqual(expect.any(Array));
+let gameboard;
+
+beforeEach(() => {
+  gameboard = createGameboard();
 });
 
-test("Gameboard array has total length of 100 elements", () => {
-  const newGameboard = createGameboard();
-  expect(newGameboard.board.flat()).toHaveLength(100);
-});
+describe("board array", () => {
+  test("Gameboard obj has gameboard array ", () => {
+    expect(gameboard.board).toEqual(expect.any(Array));
+  });
 
-test("placeShip method adds ship to array index 5, 5", () => {
-  const newGameboard = createGameboard();
-  newGameboard.placeShip(5, 5, 2, "horizontal");
-  expect(newGameboard.board[5][5]).toMatchObject({
-    length: 2,
-    totalHits: 0,
-    hasSunk: false,
+  test("Gameboard array has total length of 100 elements", () => {
+    expect(gameboard.board.flat()).toHaveLength(100);
   });
 });
 
-test("placeShip method adds ship to array index 5, 5 and 5, 6", () => {
-  const newGameboard = createGameboard();
-  newGameboard.placeShip(5, 5, 2, "horizontal");
-  expect(newGameboard.board[5][5]).toMatchObject({
-    length: 2,
-    totalHits: 0,
-    hasSunk: false,
+describe("placeShip", () => {
+  test("ship added to array index [5][5] and [5][6]", () => {
+    gameboard.placeShip(5, 5, 2, "horizontal");
+
+    expect(gameboard.board[5][5]).toMatchObject({
+      length: 2,
+      totalHits: 0,
+      hasSunk: false,
+    });
+
+    expect(gameboard.board[5][6]).toMatchObject({
+      length: 2,
+      totalHits: 0,
+      hasSunk: false,
+    });
   });
-  expect(newGameboard.board[5][6]).toMatchObject({
-    length: 2,
-    totalHits: 0,
-    hasSunk: false,
+});
+
+describe("receiveAttack", () => {
+  test("totalHits increased by 1 for attack on ship", () => {
+    gameboard.placeShip(5, 5, 2, "horizontal");
+    gameboard.receiveAttack(5, 5);
+    expect(gameboard.board[5][5].totalHits).toEqual(1);
+  });
+
+  test("co-ordinates [1,1] added to missedShots array", () => {
+    gameboard.receiveAttack(1, 1);
+    expect(gameboard.missedShots).toEqual(expect.arrayContaining([[1, 1]]));
   });
 });
 
-test("receiveAttack increases totalHits to 1 for ship at co-ordinates 5, 5", () => {
-  const newGameboard = createGameboard();
-  newGameboard.placeShip(5, 5, 2, "horizontal");
-  newGameboard.receiveAttack(5, 5);
-  expect(newGameboard.board[5][5].totalHits).toEqual(1);
-});
+describe("checkAllShipsSunk", () => {
+  test("returns true when all ships have sunk", () => {
+    gameboard.placeShip(1, 1, 2, "horizontal");
+    gameboard.receiveAttack(1, 1);
+    gameboard.receiveAttack(1, 2);
 
-test("receiveAttack adds co-ordinates 1, 1 to missedShots", () => {
-  const newGameboard = createGameboard();
-  newGameboard.receiveAttack(1, 1);
-  expect(newGameboard.missedShots).toEqual(expect.arrayContaining([[1, 1]]));
-});
+    expect(gameboard.checkAllShipsSunk()).toBeTruthy();
+  });
 
-test("checkAllShipsSunk returns true when all ships have sunk", () => {
-  const newGameboard = createGameboard();
+  test("returns false when not all ships have sunk", () => {
+    gameboard.placeShip(1, 1, 2, "horizontal");
+    gameboard.placeShip(9, 5, 2, "vertical");
+    gameboard.placeShip(5, 7, 2, "horizontal");
+    gameboard.receiveAttack(1, 1);
+    gameboard.receiveAttack(1, 2);
 
-  newGameboard.placeShip(1, 1, 2, "horizontal");
-
-  newGameboard.receiveAttack(1, 1);
-  newGameboard.receiveAttack(1, 2);
-
-  expect(newGameboard.checkAllShipsSunk()).toBeTruthy();
-});
-
-test("checkAllShipsSunk returns false when not all ships have sunk", () => {
-  const newGameboard = createGameboard();
-
-  newGameboard.placeShip(1, 1, 2, "horizontal");
-  newGameboard.placeShip(9, 5, 2, "vertical");
-  newGameboard.placeShip(5, 7, 2, "horizontal");
-
-  newGameboard.receiveAttack(1, 1);
-  newGameboard.receiveAttack(1, 2);
-
-  expect(newGameboard.checkAllShipsSunk()).toBeFalsy();
+    expect(gameboard.checkAllShipsSunk()).toBeFalsy();
+  });
 });
