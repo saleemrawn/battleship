e228aa99fd03f6b8e03342a0c138fb08e4f47c71
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-10-16 10:09:23 +0100

    test: refactor tests, update import path
    - add beforeEach to set up ship object before tests

--- test/ship.test.js
+++ test/ship.test.js
@@ -1,34 +1,34 @@
-import createShip from "../src/ship";
+import createShip from "../src/js/ship";
+
+let ship;
+
+beforeEach(() => {
+  ship = createShip(2);
+});
 
 test("Ship obj has length property with value 2", () => {
-  const newShip = createShip(2);
-  expect(newShip).toHaveProperty("length", 2);
+  expect(ship).toHaveProperty("length", 2);
 });
 
 test("Ship obj has totalHits property initialised to 0", () => {
-  const newShip = createShip(2);
-  expect(newShip).toHaveProperty("totalHits", 0);
+  expect(ship).toHaveProperty("totalHits", 0);
 });
 
 test("Ship obj has hasSunk property initialised to false", () => {
-  const newShip = createShip(2);
-  expect(newShip).toHaveProperty("hasSunk", false);
+  expect(ship).toHaveProperty("hasSunk", false);
 });
 
 test("Ship obj hit() method increases totalHits to 1", () => {
-  const newShip = createShip(2);
-  newShip.hit();
-  expect(newShip.totalHits).toEqual(1);
+  ship.hit();
+  expect(ship.totalHits).toEqual(1);
 });
 
 test("Ship obj isSunk() method returns false if totalHits does not equal ship length", () => {
-  const newShip = createShip(2);
-  expect(newShip.isSunk()).toBeFalsy();
+  expect(ship.isSunk()).toBeFalsy();
 });
 
 test("Ship obj isSunk() method returns true if totalHits equals ship length", () => {
-  const newShip = createShip(2);
-  newShip.hit();
-  newShip.hit();
-  expect(newShip.isSunk()).toBeTruthy();
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBeTruthy();
 });
