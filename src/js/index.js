b2fe9f82770e75903730e3940e0fa19e8f13b9a2
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-10-22 14:07:00 +0100

    feat: add JS entry point with dependency, objects setup

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+import { createShipPlacementService } from "./services/shipPlacementService";
+import { createShipPlacementController } from "./controllers/shipPlacementController";
+import { createShipPlacementUI } from "./ui/shipPlacementUI";
+import { createGameboardGeneratorService } from "./services/gameboardGeneratorService";
+import { createGameEventController } from "./controllers/gameEventController";
+import { createGameboardUI } from "./ui/gameboardUI";
+import createPlayer from "./player";
+import { createFormsUI } from "./ui/formsUI";
+import { createModalUI } from "./ui/modalUI";
+import { loadEventListeners } from "./event-listeners";
+import "../css/main.css";
+
+const gameboardGeneratorService = createGameboardGeneratorService();
+const shipPlacementService = createShipPlacementService();
+const shipPlacementUI = createShipPlacementUI();
+const gameboardUI = createGameboardUI();
+const formsUI = createFormsUI();
+const modalUI = createModalUI();
+
+export const shipPlacementController = createShipPlacementController({
+  service: shipPlacementService,
+  generator: gameboardGeneratorService,
+  shipPlacementUI: shipPlacementUI,
+  formsUI: formsUI,
+  gameboardUI: gameboardUI,
+});
+
+export const gameEventController = createGameEventController({
+  shipPlacementUI: shipPlacementUI,
+  gameboardUI: gameboardUI,
+  formsUI: formsUI,
+  modalUI: modalUI,
+});
+
+function init() {
+  let human = createPlayer(1, "Player");
+  let computer = createPlayer(2, "Computer");
+  loadEventListeners(human, computer);
+}
+
+init();
